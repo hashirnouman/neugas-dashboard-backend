596d9adff6cdf52e77973453bbc79d500ad0a445
hashirnouman <hashirnouman76@gmail.com>
2023-10-16 16:13:27 +0500

    planetscale database connected

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express, { Express, Request, Response, Application } from "express";
 import * as dotenv from "dotenv";
-
+import { PrismaClient } from "@prisma/client";
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
   res.send("Hello, Express with TypeScript! helo");
 });
-app.listen(port, () => {
+app.listen(port, async () => {
+  const data = await prisma.users.updateMany({
+    where: { first_name: "hashir" },
+    data: { last_name: "nouman" },
+  });
+  console.log(data);
   console.log(`Server is Fire at http://localhost:${port}`);
 });
